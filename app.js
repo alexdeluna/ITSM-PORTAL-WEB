71116b1d37b771064d1017796e43c349c8e874b3
Alex de Luna <67716472+alexdeluna@users.noreply.github.com>
2026-09-08 16:23:19 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+import { auth } from "./auth.js";
+
+
+const loginScreen = document.getElementById("loginScreen");
+const registerScreen = document.getElementById("registerScreen");
+const appScreen = document.getElementById("appScreen");
+
+const loginEmail = document.getElementById("loginEmail");
+const loginPassword = document.getElementById("loginPassword");
+
+const registerEmail = document.getElementById("registerEmail");
+const registerPassword = document.getElementById("registerPassword");
+
+const loginMessage = document.getElementById("loginMessage");
+const registerMessage = document.getElementById("registerMessage");
+
+const userEmail = document.getElementById("userEmail");
+
+
+document.getElementById("btnLogin").addEventListener("click", async () => {
+
+    loginMessage.textContent = "";
+
+    try {
+
+        await signInWithEmailAndPassword(
+            auth,
+            loginEmail.value.trim(),
+            loginPassword.value
+        );
+
+    } catch (error) {
+
+        loginMessage.textContent = "E-mail ou senha inválidos.";
+
+        console.error(error);
+
+    }
+
+});
+
+
+document.getElementById("btnCadastro").addEventListener("click", async () => {
+
+    registerMessage.textContent = "";
+
+    try {
+
+        await createUserWithEmailAndPassword(
+            auth,
+            registerEmail.value.trim(),
+            registerPassword.value
+        );
+
+    } catch (error) {
+
+        registerMessage.textContent = error.message;
+
+        console.error(error);
+
+    }
+
+});
+
+
+document.getElementById("btnMostrarCadastro").addEventListener("click", () => {
+
+    loginScreen.classList.add("hidden");
+    registerScreen.classList.remove("hidden");
+
+});
+
+
+document.getElementById("btnVoltarLogin").addEventListener("click", () => {
+
+    registerScreen.classList.add("hidden");
+    loginScreen.classList.remove("hidden");
+
+});
+
+
+document.getElementById("btnLogout").addEventListener("click", async () => {
+
+    await signOut(auth);
+
+});
+
+
+onAuthStateChanged(auth, (user) => {
+
+    if (user) {
+
+        loginScreen.classList.add("hidden");
+        registerScreen.classList.add("hidden");
+        appScreen.classList.remove("hidden");
+
+        userEmail.textContent = user.email;
+
+    } else {
+
+        appScreen.classList.add("hidden");
+        registerScreen.classList.add("hidden");
+        loginScreen.classList.remove("hidden");
+
+        userEmail.textContent = "";
+
+    }
+
+});
---
+++ config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB0uicrOVFPK1A0rmusCmaGt7c_tvzoTas",
+    authDomain: "itsm-portal-19a9d.firebaseapp.com",
+    projectId: "itsm-portal-19a9d",
+    storageBucket: "itsm-portal-19a9d.firebasestorage.app",
+    messagingSenderId: "828831859574",
+    appId: "1:828831859574:web:ea3f81c02fa30cf2f93057"
+};
+
+export const app = initializeApp(firebaseConfig);
